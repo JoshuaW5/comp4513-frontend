bdf0402278255733a3f1978118c74dd724ef48bc
Joshua Wojak <joshuawojak@gmail.com>
2018-04-09 21:29:44 -0600

    login changes

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -47,7 +47,7 @@ class Login extends Component {
             }
 
   render() {
-      if (this.state.authenticated) {return <Redirect to='/home'/>;}
+      if (this.state.authenticated) {return <Redirect to='/'/>;}
     return (
       <div>
                         <section className="hero is-light">
